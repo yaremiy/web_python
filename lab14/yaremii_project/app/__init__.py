4d811b01c131d52757f411928134997315f7bdb3
Hanna Yaremii <anyta070403@gmail.com>
2022-12-20 15:20:06 +0200

    lab14 - done

---
+++ lab14/yaremii_project/app/__init__.py
@@ -0,0 +1,45 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_ckeditor import CKEditor
+from flask_jwt_extended import JWTManager
+from config import config
+
+db = SQLAlchemy()
+migrate = Migrate()
+login_manager = LoginManager()
+ckeditor = CKEditor()
+jwt = JWTManager()
+login_manager.login_view = "account.login"
+login_manager.login_message_category = "info"
+
+
+def create_app(config_name = 'default'):
+    app = Flask(__name__)
+    app.config.from_object(config.get(config_name))
+
+    db.init_app(app)
+    migrate.init_app(app, db, render_as_batch=True)
+    login_manager.init_app(app)
+    ckeditor.init_app(app)
+    jwt.init_app(app)
+
+    with app.app_context():
+        from app.home import home_bp
+        from app.contact import contact_bp
+        from app.account import account_bp
+        from app.task import task_bp
+        from app.category_api import category_api_bp
+        from app.task_api import task_api_bp
+        from app.swagger import swagger_bp
+
+        app.register_blueprint(home_bp)
+        app.register_blueprint(contact_bp)
+        app.register_blueprint(account_bp)
+        app.register_blueprint(task_bp)
+        app.register_blueprint(category_api_bp, url_prefix='/api')
+        app.register_blueprint(task_api_bp, url_prefix='/api/v2/tasks')
+        app.register_blueprint(swagger_bp, url_prefix='/swagger')
+
+    return app
